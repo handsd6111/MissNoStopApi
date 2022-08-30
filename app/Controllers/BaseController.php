908ccf0266fb23abf35765f5188844e556829b1e
19103105 <s19103105@stu.edu.tw>
2022-08-30 06:39:52 +0000

    「取得指定捷運系統、路線、經度及緯度的最近捷運車站」API 已可使用。

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,6 +7,7 @@ use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use Exception;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -48,6 +49,51 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
+    }
+
+    /**
+     * 參數驗證規則：$metroValidationRules[參數名稱] = 規則
+     */
+    protected $metroValidationRules = [
+        "systemId"     => "alpha|max_length[4]",
+        "routeId"      => "alpha_numeric_punct|max_length[12]",
+        "stationId"    => "alpha_numeric_punct|max_length[12]",
+        "endStationId" => "alpha_numeric_punct|max_length[12]",
+        "longitude"    => "alpha_numeric_punct|max_length[12]",
+        "latitude"     => "alpha_numeric_punct|max_length[12]"
+    ];
+
+    /**
+     * 驗證參數
+     * @param array $vData 參數陣列
+     * @param bool 驗證成功與否
+     */
+    public function metro_validation($vData)
+    {
+        try
+        {
+            // 參數名稱
+            $vDataKeys = array_keys($vData);
+            // 此次所需的驗證規則
+            $vRules = [];
+
+            // 對照 $this->metroValidationRules 的驗證資料及 $vData 的參數名稱，並寫入 $vRules
+            for ($i = 0; $i < sizeof($vData); $i++)
+            {
+                $vRule = $vDataKeys[$i];
+                $vRules[$vRule] = $this->metroValidationRules[$vRule];
+            }
+
+            if (!$this->validateData($vData, $vRules))
+            {
+                return false;
+            }
+            return true;
+        }
+        catch (Exception $e)
+        {
+            throw $e;
+        }
     }
 
     public function send_response($data = [], $status = 200, $message = "OK", $headers = []) {
