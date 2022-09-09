9f3eef7fb59e3d5aae101a8601f191cb7b2e0008
handsd6111 <handsd6111@gmail.com>
2022-09-09 11:26:08 +0000

    thsr orm model

---
+++ app/Models/ORM/ThsrStationModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\ORM;
+
+use CodeIgniter\Model;
+
+class ThsrStationModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'THSR_stations';
+    protected $primaryKey       = 'HS_id';
+    protected $useAutoIncrement = false;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'HS_id',
+        'HS_name_TC',
+        'HS_name_EN',
+        'HS_city_id',
+        'HS_longitude',
+        'HS_latitude'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
