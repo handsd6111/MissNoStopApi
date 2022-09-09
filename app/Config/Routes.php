45d35f3160ab9e637c8cde6c0105e5115345b326
handsd6111 <handsd6111@gmail.com>
2022-09-09 11:54:56 +0000

    因應檔名修改Route的Controller名稱

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -64,24 +64,24 @@ $routes->group('tdx', static function ($routes) {
 
     // tdx/data
     $routes->group('data', static function ($routes) {
-        $routes->cli('cities', 'TDXDataController::getAndSetCities'); // tdx/data/cities 城市資料
+        $routes->cli('cities', 'TdxBaseController::getAndSetCities'); // tdx/data/cities 城市資料
 
         //tdx/data/metro
         $routes->group('metro', static function ($routes) {
 
             //tdx/data/metro/route
-            $routes->cli('route/all', 'TDXDataController::setMetroRouteAll'); // 全部捷運系統路線
-            $routes->cli('route/(:alphanum)', 'TDXDataController::setMetroRoute/$1'); // 單筆捷運系統的路線
+            $routes->cli('route/all', 'TdxMetroController::setMetroRouteAll'); // 全部捷運系統路線
+            $routes->cli('route/(:alphanum)', 'TdxMetroController::setMetroRoute/$1'); // 單筆捷運系統的路線
 
-            $routes->cli('station/all', 'TDXDataController::setMetroStationAll'); // 全部捷運系統的站點
-            $routes->cli('station/(:alphanum)', 'TDXDataController::setMetroStation/$1'); // 單個捷運系統的站點 
+            $routes->cli('station/all', 'TdxMetroController::setMetroStationAll'); // 全部捷運系統的站點
+            $routes->cli('station/(:alphanum)', 'TdxMetroController::setMetroStation/$1'); // 單個捷運系統的站點 
 
-            $routes->cli('duration/(:alphanum)', 'TDXDataController::setMetroDuration/$1'); // 單個捷運系統的運行時間，不包含 TYMC (桃捷)
+            $routes->cli('duration/(:alphanum)', 'TdxMetroController::setMetroDuration/$1'); // 單個捷運系統的運行時間，不包含 TYMC (桃捷)
 
-            $routes->cli('routeStation/all', 'TDXDataController::setMetroRouteStationAll'); // 全部捷運系統車站與路線的關聯資料
-            $routes->cli('routeStation/(:alphanum)', 'TDXDataController::setMetroRouteStation/$1'); // 單個捷運系統車站與路線的關聯資料
+            $routes->cli('routeStation/all', 'TdxMetroController::setMetroRouteStationAll'); // 全部捷運系統車站與路線的關聯資料
+            $routes->cli('routeStation/(:alphanum)', 'TdxMetroController::setMetroRouteStation/$1'); // 單個捷運系統車站與路線的關聯資料
 
-            $routes->cli('arrival/(:alphanum)', 'TDXDataController::setMetroArrival/$1'); // 單個捷運系統的時刻表
+            $routes->cli('arrival/(:alphanum)', 'TdxMetroController::setMetroArrival/$1'); // 單個捷運系統的時刻表
         });
     });
 });
